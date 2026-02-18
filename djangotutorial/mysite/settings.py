5fec088e19bcbbba48d6dae91188915a2e0c015f
Hardik-Bhochiya <ahirhardik027@gmail.com>
2026-02-18 23:56:37 +0530

    add css and Bootstrap file

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -36,9 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'polls',
+    'django.contrib.staticfiles',   
+    'polls',                        
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
